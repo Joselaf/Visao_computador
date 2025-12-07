d1ffe760ed31869f29b4400d06921c7f1833fa21
José Luís Alves Fernandes <luis.jlaf@hotmail.com>
2025-12-07 15:31:20 +0000

    Correção de erro

--- PI.py
+++ PI.py
@@ -8,7 +8,7 @@ def folder(folder):
    for filename in os.listdir(folder):
          Path = os.path.join(folder,filename) # cria o caminho completo do ficheiro
          img = cv.imread(Path)  # lê a imagem 
-         img_with_obj = find_obj(img)  # cshama a função para encontrar o objeto na imagem
+         img_with_obj = find_obj(img)  # chama a função para encontrar o objeto na imagem
          plot_two_images(img, img_with_obj)  # plota as duas imagens lado a lado
 
 
