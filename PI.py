4c2bebf294267f98aac7a12db71588e5f24ae4cf
José Luís Alves Fernandes <luis.jlaf@hotmail.com>
2025-12-07 15:33:58 +0000

    Consistência

--- PI.py
+++ PI.py
@@ -6,10 +6,10 @@ import os
 
 def folder(folder):
    for filename in os.listdir(folder):
-         Path = os.path.join(folder,filename) # cria o caminho completo do ficheiro
-         img = cv.imread(Path)  # lê a imagem 
-         img_with_obj = find_obj(img)  # chama a função para encontrar o objeto na imagem
-         plot_two_images(img, img_with_obj)  # plota as duas imagens lado a lado
+         Path = os.path.join(folder,filename) ## cria o caminho completo do ficheiro
+         img = cv.imread(Path)  ## lê a imagem 
+         img_with_obj = find_obj(img)  ## chama a função para encontrar o objeto na imagem
+         plot_two_images(img, img_with_obj)  ## plota as duas imagens lado a lado
 
 
 def file(img):
@@ -18,26 +18,26 @@ def file(img):
 
 
 def find_obj(img):
-   img_copy = img.copy() # cria uma cópia da imagem original para desenhar o retângulo
+   img_copy = img.copy() ## cria uma cópia da imagem original para desenhar o retângulo
    height,width = img.shape[:2] ## Obtém as dimensões da imagem
    x_CE = width // 4  ## Calcula as coordenadas do canto superior esquerdo do retângulo
    y_CE = height // 4 ## Calcula as coordenadas do canto superior esquerdo do retângulo
    x_CD = x_CE + width // 2 ## Calcula as coordenadas do canto inferior direito do retângulo
    Y_CD = y_CE  + height// 2 ## Calcula as coordenadas do canto inferior direito do retângulo
-   cv.rectangle(img_copy, (x_CE,y_CE),(x_CD, Y_CD),(255,0,0),3) # desenha um retângulo azul na imagem
+   cv.rectangle(img_copy, (x_CE,y_CE),(x_CD, Y_CD),(255,0,0),3) ## desenha um retângulo azul na imagem
    return img_copy
 
 
 def plot_two_images(img,img_copy):
-   plt.subplot(121)
-   plt.imshow(img)
-   plt.title('Imagem Original')
-   plt.axis('off')
-   plt.subplot(122)
-   plt.imshow(img_copy)
-   plt.title('Imagem com Objeto Encontrado')
-   plt.axis('off')
-   plt.show()
+   plt.subplot(121) ## Cria uma figura com duas subplots
+   plt.imshow(img) ## Mostra a imagem original
+   plt.title('Imagem Original') ## Adiciona um título à imagem
+   plt.axis('off') ## Remove os eixos da imagem
+   plt.subplot(122) ## Seleciona a segunda subplot
+   plt.imshow(img_copy) ## Mostra a imagem com o objeto encontrado
+   plt.title('Imagem com Objeto Encontrado') ## Adiciona um título à imagem
+   plt.axis('off') ## Remove os eixos da imagem
+   plt.show() ## Exibe as duas imagens lado a lado
 
 
 def main():
@@ -46,11 +46,11 @@ def main():
        return
    Folder_or_File = sys.argv[1]
    if os.path.isdir(Folder_or_File): ##Verifica se argumento é uma pasta
-       folder(Folder_or_File)
+       folder(Folder_or_File) ## chama a função para processar a pasta
    elif os.path.isfile(Folder_or_File): ##Verifica se argumento é um ficheiro
-         img = cv.imread(Folder_or_File)  # lê a imagem
-         file(img)
+         img = cv.imread(Folder_or_File)  ## lê a imagem
+         file(img)  ## chama a função para processar o ficheiro
         
 if __name__ == "__main__": ## Verifica se o script está a ser executado diretamente
-    main() # executa a secção do programa  main
+    main() ## executa a secção do programa  main
 
