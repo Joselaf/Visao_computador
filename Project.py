044c4f3c6c0aeb162e1d43bb5401abcf00b82233
José Luís Alves Fernandes <luis.jlaf@hotmail.com>
2026-01-19 13:47:38 +0000

    Project

---
+++ Project.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('Img.png')
+
+img_hsv  = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+img_HSV_V = img_hsv[:,:,2]
+img_HSV_S = img_hsv[:,:,1]
+
+
+plt.hist(img_HSV_V.ravel(),256,[0,256])
+plt.show()
+
+
+cv.imshow('Image', img_HSV_V)
+cv.waitKey(0)  
+cv.destroyAllWindows()
