949f895e14db81ed6d01c21c53a4773b45cc0505
José Luís Alves Fernandes <luis.jlaf@hotmail.com>
2026-01-19 22:05:12 +0000

    Needs help

--- Project.py
+++ Project.py
@@ -2,35 +2,32 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('Img.png')
+A = cv.imread('Img.png')
 
-img_hsv  = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-img_HSV_V = img_hsv[:,:,2]
-img_HSV_S = img_hsv[:,:,1]
+HSV = cv.cvtColor(A, cv.COLOR_BGR2HSV)
+HSV_S = HSV[:,:,1]
+HSV_V = HSV[:,:,2]
 
-hist = cv.calcHist([img],[0], None, [256], [0, 256])
+hist = cv.calcHist([A],[0], None, [256], [0, 256])
 
-plt.hist(img.ravel(), 256, [0, 256])
+plt.hist(A.ravel(), 256, [0, 256])
 plt.show()
 
-img_hsv_h = img_hsv[:, :, 0]
-img_hsv_S = img_hsv[:, :, 1]
-img_hsv_V = img_hsv[:, :, 2]
-HSV_S_eq = cv.equalizeHist(img_hsv_S)
-HSV_V_eq = cv.equalizeHist(img_hsv_V) 
+HSV_S_eq = cv.equalizeHist(HSV_S)
+HSV_V_eq = cv.equalizeHist(HSV_V) 
 
-HSV_eq = np.zeros(img_hsv.shape, np.uint8)
+HSV_eq = np.zeros(HSV.shape, np.uint8)
 
-HSV_eq[:, :, 0] = img_hsv_h
+HSV_eq[:, :, 0] = HSV[:, : ,0]
 HSV_eq[:, :, 1] = HSV_S_eq
 HSV_eq[:, :, 2] = HSV_V_eq
 
 HSV_eq = cv.GaussianBlur(HSV_eq, (5,5), 0)
 B = cv.cvtColor(HSV_eq, cv.COLOR_HSV2BGR)
-B = cv.blur(B, (5,5))
+B = cv.GaussianBlur(B, (5,5), 0)
 
 plt.subplot(121)
-plt.imshow(img)
+plt.imshow(A)
 plt.title("Imagem original")
 plt.axis('off')
 plt.subplot(122)
