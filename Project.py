1b64151f62f8c0007fc6535c8f4ee2f794400065
José Luís Alves Fernandes <luis.jlaf@hotmail.com>
2026-01-19 21:58:16 +0000

    Needs help

--- Project.py
+++ Project.py
@@ -12,3 +12,32 @@ hist = cv.calcHist([img],[0], None, [256], [0, 256])
 
 plt.hist(img.ravel(), 256, [0, 256])
 plt.show()
+
+img_hsv_h = img_hsv[:, :, 0]
+img_hsv_S = img_hsv[:, :, 1]
+img_hsv_V = img_hsv[:, :, 2]
+HSV_S_eq = cv.equalizeHist(img_hsv_S)
+HSV_V_eq = cv.equalizeHist(img_hsv_V) 
+
+HSV_eq = np.zeros(img_hsv.shape, np.uint8)
+
+HSV_eq[:, :, 0] = img_hsv_h
+HSV_eq[:, :, 1] = HSV_S_eq
+HSV_eq[:, :, 2] = HSV_V_eq
+
+HSV_eq = cv.GaussianBlur(HSV_eq, (5,5), 0)
+B = cv.cvtColor(HSV_eq, cv.COLOR_HSV2BGR)
+B = cv.blur(B, (5,5))
+
+plt.subplot(121)
+plt.imshow(img)
+plt.title("Imagem original")
+plt.axis('off')
+plt.subplot(122)
+plt.imshow(B)
+plt.title("Imagem melhorada")
+plt.axis('off')
+plt.show()
+cv.waitKey(0)
+cv.destroyAllWindows()
+
